b3be6b54d1130550b9a026279a3bfe0490645aab
manuel2001bk <61120758+manuel2001bk@users.noreply.github.com>
2022-07-10 13:09:02 -0500

    añadida vista

---
+++ main.py
@@ -0,0 +1,16 @@
+from ventana_ui import *
+
+class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QMainWindow.__init__(self, *args, **kwargs)
+        self.setupUi(self)
+
+    
+    def actualizar(self):
+        self.label.setText("¡Acabas de hacer clic en el botón!")
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
